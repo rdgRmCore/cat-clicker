176810402a1fef73ec1dea7d04f83e680e62e575
rdg <rdg@rmcore.com>
2015-07-14 11:17:11 -0500

    Add a list of cats on the left hand side of the page.

--- js/clicks.js
+++ js/clicks.js
@@ -1,17 +1,42 @@
 // Whole-script strict mode syntax
 "use strict";
 
+
+function Cat(Name){
+  this.name = Name;
+  this.NumClicks = 0;
+}
+
+var cats = [
+  new Cat("chewie"),
+  new Cat("furry"),
+  new Cat("kitten"),
+  new Cat("sockington"),
+  new Cat("watson")
+];
+
 var numClicks = [0,0];
 $(document).ready(function(){
-  $("#kitten-img").click(function(e) {
-    numClicks[0]++;
-    $("#kitten-clicks").text("Num clicks: " + numClicks[0]);
+  displayCatList();
+
+  //$("#kitten-img").click(function(e) {
+    //numClicks[0]++;
+    //$("#kitten-clicks").text("Num clicks: " + numClicks[0]);
     
-  });
-  $("#chewie-img").click(function(e) {
-    numClicks[1]++;
-    $("#chewie-clicks").text("Num clicks: " + numClicks[1]);
+  //});
+  //$("#chewie-img").click(function(e) {
+    //numClicks[1]++;
+    //$("#chewie-clicks").text("Num clicks: " + numClicks[1]);
     
-  });
+  //});
 });
+
+function displayCatList(){
+  var cat;
+  for (cat in cats){
+    var catLi = "<li>" + cats[cat].name + "</li>";
+    $("#cat-list").append(catLi);
+  }
+}
+
 
