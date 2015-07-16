1070950ec41f5818c93c1e286cf8414007fde316
rdg <rdg@rmcore.com>
2015-07-16 11:11:07 -0500

    Refactor for separation of concerns.

--- js/clicks.js
+++ js/clicks.js
@@ -2,59 +2,91 @@
 "use strict";
 
 
-function Cat(Name){
-  this.name = Name;
-  this.numClicks = 0;
-}
+var Model = {
 
-var cats = [
-  new Cat("chewie"),
-  new Cat("furry"),
-  new Cat("kitten"),
-  new Cat("sockington"),
-  new Cat("watson")
-];
+    cats: [
+      {"name": "chewie",     "numClicks": 0},
+      {"name": "furry",      "numClicks": 0},
+      {"name": "kitten",     "numClicks": 0},
+      {"name": "sockington", "numClicks": 0},
+      {"name": "watson",     "numClicks": 0}
+    ],
 
-var numClicks = [0,0];
-$(document).ready(function(){
-  displayCatList();
-  
-  //use jQuery selector to loop through each element of the list
-  $("#cat-list li").each(function (index) {
-    console.log(index + ": " + $(this).text());
-    //set a click handler for each name in the list
-    $(this).click(function(e){
-      var name = $(this).text();
-      var fileName = "img/" + name + ".jpg";
-      $("#cat-name").text(name);
-      $("#cat-img").attr("src",fileName);
-      $("#cat-clicks").text("Num clicks: " + cats[index].numClicks);
-    });
-
-  });
-
-  $("#cat-img").click(function(e) {
-    var name = $("#cat-name").text();
-    console.log(name);
-
+  GetAllCats: function(){
+    return Model.cats;
+  },
+  GetNumClicks: function(index){
+    return Model.cats[index].numClicks;
+  },
+  IncrementNumClicks: function(index){
+    Model.cats[index].numClicks++;
+  },
+  FindIndex: function(name){
     var cat;
-    for (cat in cats){
-      if (cats[cat].name == name){
-        console.log("Found index : " + cat);
-        cats[cat].numClicks++;
-        $("#cat-clicks").text("Num clicks: " + cats[cat].numClicks);
+    for (cat in Model.cats){
+      if (Model.cats[cat].name == name){
+        return cat;
       }
     }
-  });
+  }
+};
 
+$(document).ready(function(){
+  Controller.Init();
 });
 
-function displayCatList(){
-  var cat;
-  for (cat in cats){
-    var catLi = "<li id=\"cat-list-item\" >" + cats[cat].name + "</li>";
-    $("#cat-list").append(catLi);
+var Controller = {
+  Init: function(){
+    CatListView.render();
+
+    //use jQuery selector to loop through each element of the cat list
+    $("#cat-list li").each(function (index) {
+      //set a click handler for each name in the list
+      $(this).click(function(e){
+        CatDetailView.render(index, $(this).text(), Model.GetNumClicks(index));
+      });
+
+    });
+
+    //Set Click handler for cat image
+    $("#cat-img").click(function(e) {
+      var name = $("#cat-name").text();
+      var index = Model.FindIndex(name);
+
+      Model.IncrementNumClicks(index);
+      CatDetailView.UpdateClicks(Model.GetNumClicks(index));
+
+    });
+  },
+
+  GetCats: function(){
+    return Model.GetAllCats();
   }
+}
+
+var CatListView = {
+  render: function(){
+    var cats = Controller.GetCats();
+    var cat;
+    for (cat in cats){
+      var catLi = "<li id=\"cat-list-item\" >" + cats[cat].name + "</li>";
+      $("#cat-list").append(catLi);
+    }
+  }
+}
+
+var CatDetailView = {
+  render: function(Index,Name,Clicks){
+        var fileName = "img/" + Name + ".jpg";
+        $("#cat-name").text(Name);
+        $("#cat-img").attr("src",fileName);
+        CatDetailView.UpdateClicks(Clicks);
+  },
+  UpdateClicks: function(NumClicks){
+    $("#cat-clicks").text("Num clicks: " + NumClicks);
+  }
+  
+
 }
 
 
